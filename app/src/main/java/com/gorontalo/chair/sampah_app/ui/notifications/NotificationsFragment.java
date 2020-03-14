3f592077e305340880c5481572dce6ad5cc0b4cc
chair <chairulharun@gmail.com>
2020-03-14 13:17:55 +0800

    update 14 maret pagi 2

--- app/src/main/java/com/gorontalo/chair/sampah_app/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/gorontalo/chair/sampah_app/ui/notifications/NotificationsFragment.java
@@ -16,20 +16,9 @@ import com.gorontalo.chair.sampah_app.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
