877d58f24241a497249a9653c6ef2eea7e10d181
chair <chairulharun@gmail.com>
2020-03-29 21:11:58 +0800

    update 29 malam

--- app/src/main/java/com/gorontalo/chair/sampah_app/UserActivity.java
+++ app/src/main/java/com/gorontalo/chair/sampah_app/UserActivity.java
@@ -68,7 +68,7 @@ public class UserActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_login) {
-            startActivity(new Intent(UserActivity.this, MainActivity.class));
+            startActivity(new Intent(UserActivity.this, LoginActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
