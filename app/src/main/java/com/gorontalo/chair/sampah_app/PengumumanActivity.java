cd68878b9e4cca86ef4ee62870fe410ba80e3120
chair <chairulharun@gmail.com>
2020-04-16 13:22:48 +0800

    update 16 april pagi

--- app/src/main/java/com/gorontalo/chair/sampah_app/PengumumanActivity.java
+++ app/src/main/java/com/gorontalo/chair/sampah_app/PengumumanActivity.java
@@ -4,20 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.gorontalo.chair.sampah_app.adapter.HttpsTrustManagerAdapter;
 import com.gorontalo.chair.sampah_app.adapter.RecycleViewPengumumanAdapter;
 import com.gorontalo.chair.sampah_app.adapter.URLAdapter;
+import com.gorontalo.chair.sampah_app.adapter.VolleyAdapter;
 import com.gorontalo.chair.sampah_app.model.PengumumanModel;
 
 import org.json.JSONArray;
@@ -25,16 +32,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PengumumanActivity extends AppCompatActivity {
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_HASIL = "hasil";
+
     List<PengumumanModel> pengumumanModelList;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager recyclerViewlayoutManager;
     RecyclerView.Adapter recyclerViewadapter;
-    ProgressBar progressBar;
-    JsonArrayRequest jsonArrayRequest ;
-    RequestQueue requestQueue ;
     View ChildView ;
     int GetItemPosition ;
     ArrayList<String> Pengumuman;
@@ -46,14 +55,12 @@ public class PengumumanActivity extends AppCompatActivity {
 
         pengumumanModelList = new ArrayList<>();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView1);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
         recyclerView.setHasFixedSize(true);
         recyclerViewlayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(recyclerViewlayoutManager);
-        progressBar.setVisibility(View.VISIBLE);
         Pengumuman = new ArrayList<>();
 
-        JSON_DATA_WEB_CALL();
+        getData();
 
         recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
             GestureDetector gestureDetector = new GestureDetector(PengumumanActivity.this, new GestureDetector.SimpleOnGestureListener() {
@@ -82,42 +89,63 @@ public class PengumumanActivity extends AppCompatActivity {
         });
     }
 
-    public void JSON_DATA_WEB_CALL(){
-        jsonArrayRequest = new JsonArrayRequest(new URLAdapter().getPengumuman(),
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        progressBar.setVisibility(View.GONE);
-                        JSON_PARSE_DATA_AFTER_WEBCALL(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                    }
-                });
+    private void getData() {
+        final ProgressDialog progressDialog = new ProgressDialog(PengumumanActivity.this);
+        progressDialog.setMessage("Mengambil Data ...");
+        progressDialog.show();
 
-        requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonArrayRequest);
-    }
+        HttpsTrustManagerAdapter.allowAllSSL();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, new URLAdapter().getPengumuman(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jObj = new JSONObject(response.toString());
+                    int success = jObj.getInt(TAG_SUCCESS);
+                    if (success == 1) {
+                        JSONArray jsonArray = jObj.getJSONArray("hasil");
+                        for(int i = 0; i<jsonArray.length(); i++) {
+                            PengumumanModel pengumumanModel = new PengumumanModel();
+                            try {
+                                JSONObject json = jsonArray.getJSONObject(i);
+                                pengumumanModel.setIsiPengumuman(json.getString("isi_pengumuman"));
+                                Pengumuman.add(json.getString("isi_pengumuman"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
 
-    public void JSON_PARSE_DATA_AFTER_WEBCALL(JSONArray array){
-        for(int i = 0; i<array.length(); i++) {
-            PengumumanModel pengumumanModel = new PengumumanModel();
-            JSONObject json = null;
-            try {
-                json = array.getJSONObject(i);
-                pengumumanModel.setIsiPengumuman(json.getString("isi_pengumuman"));
-                Pengumuman.add(json.getString("isi_pengumuman"));
-            } catch (JSONException e) {
-                e.printStackTrace();
+                            pengumumanModelList.add(pengumumanModel);
+                        }
+
+                        recyclerViewadapter = new RecycleViewPengumumanAdapter(pengumumanModelList, getApplicationContext());
+                        recyclerView.setAdapter(recyclerViewadapter);
+
+                        progressDialog.dismiss();
+                    } else {
+                        Toast.makeText(getApplicationContext(), jObj.getString(TAG_HASIL), Toast.LENGTH_LONG).show();
+                        progressDialog.dismiss();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    progressDialog.dismiss();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+
+                return params;
             }
 
-            pengumumanModelList.add(pengumumanModel);
-        }
+        };
 
-        recyclerViewadapter = new RecycleViewPengumumanAdapter(pengumumanModelList, this);
-        recyclerView.setAdapter(recyclerViewadapter);
-
+        VolleyAdapter.getInstance().addToRequestQueue(stringRequest, "json_pekerjaan");
     }
 }
